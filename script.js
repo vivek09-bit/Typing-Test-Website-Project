3a7a410c4f7e44bbdd41b57178167fb420e5c1d0
Vivek Gupta <vkg1257@gmai.com>
2024-09-30 22:09:55 +0530

    Staring Commit with initial HTML, CSS, JS  Files

---
+++ script.js
@@ -0,0 +1,9 @@
+let TypingArea = document.getElementById('Typing-Area');
+let Button = document.getElementById('Start');
+let Speed = document.getElementById('Speed');
+let Accuracy = document.getElementById('Accuracy');
+
+Button.addEventListener('click',()=>{
+    
+    alert(TypingArea.value)
+})
