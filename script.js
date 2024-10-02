afbf7c04f8d7cd57db97e1aecd1ac9044b6e6a04
Vivek Gupta <vkg1257@gmai.com>
2024-10-02 23:02:49 +0530

    2ND update

--- script.js
+++ script.js
@@ -3,7 +3,24 @@ let Button = document.getElementById('Start');
 let Speed = document.getElementById('Speed');
 let Accuracy = document.getElementById('Accuracy');
 
-Button.addEventListener('click',()=>{
-    
-    alert(TypingArea.value)
+// TypingArea.addEventListener('focus',setInterval(()=>{
+//     Speed.textContent = TypingArea.value.length;
+
+// }, 1000))
+
+// TypingArea.addEventListener('keypress',setInterval(()=>{
+//     let StartTime =  new Date()
+//     Accuracy.textContent =  StartTime;
+//     Speed.textContent = TypingArea.value.length;
+// }, 2);
+
+TypingArea.addEventListener('focus',()=>{
+    const StartTime = Date.now();
+    setInterval(()=>{
+        let currentTime = (Date.now()-StartTime)/60000;
+        let TypedWords = TypingArea.value.trim().split(/\s+/).length;
+        let CharSpeed = Math.round(TypedWords / currentTime);
+        Accuracy.textContent = Math.round(CharSpeed);
+        Speed.textContent = TypedWords;
+    },1000)
 })
